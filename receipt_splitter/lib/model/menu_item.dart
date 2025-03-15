5522341d4be372061ca649705092840d091b1707
Wunna Kyaw <wunnakyaw.developer@gmail.com>
2025-03-15 11:55:25 +0630

    fix table widget to align with UI

--- receipt_splitter/lib/model/menu_item.dart
+++ receipt_splitter/lib/model/menu_item.dart
@@ -1,15 +1,19 @@
+import 'package:receipt_splitter/model/participant.dart';
+
 class MenuItem {
   final int id;
   final String name;
   final int quantity;
   final double price;
   double? total;
+  List<Participant> participants;
 
   MenuItem({
     required this.id,
     required this.name,
     required this.quantity,
     required this.price,
+    this.participants = const [],
   }) : total = _calculateTotalAmount(price, quantity);
 
   static double _calculateTotalAmount(double price, int quantity) => price * quantity;
--- receipt_splitter/lib/module/receipt_detail/common/table_widget.dart
+++ receipt_splitter/lib/module/receipt_detail/common/table_widget.dart
@@ -17,10 +17,34 @@ class TableWidget extends StatelessWidget {
         // Header Row
         Row(
           children: [
-            Expanded(flex: 1, child: columnWithPadding(child: Text(QTY, style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.end), vertical: 0),),
-            Expanded(flex: 3, child: columnWithPadding(child: Text(NAME, style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.start), vertical: 0)),
-            Expanded(flex: 2, child: columnWithPadding(child: Text(TOTAL, style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.center), vertical: 0)),
-            Expanded(flex: 2, child: columnWithPadding(child: Text(actionName, style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.center), vertical: 0)),
+            Expanded(
+              flex: 1,
+              child: columnWithPadding(
+                child: Text(QTY, style: Theme.of(context).textTheme.labelSmall?.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant), textAlign: TextAlign.end),
+                vertical: 0,
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: columnWithPadding(
+                child: Text(NAME, style: Theme.of(context).textTheme.labelSmall?.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant), textAlign: TextAlign.start),
+                vertical: 0,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: columnWithPadding(
+                child: Text(TOTAL, style: Theme.of(context).textTheme.labelSmall?.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant), textAlign: TextAlign.center),
+                vertical: 0,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: columnWithPadding(
+                child: Text(actionName, style: Theme.of(context).textTheme.labelSmall?.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant), textAlign: TextAlign.center),
+                vertical: 0,
+              ),
+            ),
           ],
         ),
         const Divider(),
@@ -30,9 +54,9 @@ class TableWidget extends StatelessWidget {
             children: [
               Row(
                 children: [
-                  Expanded(flex: 1, child: columnWithPadding(child: Text(qtyFormatter.format(item.quantity), style: Theme.of(context).textTheme.labelMedium, textAlign: TextAlign.end))),
-                  Expanded(flex: 3, child: columnWithPadding(child: Text(item.name, style: Theme.of(context).textTheme.labelMedium, textAlign: TextAlign.start))),
-                  Expanded(flex: 2, child: columnWithPadding(child: Text(amountFormatter.format(item.total), style: Theme.of(context).textTheme.labelMedium, textAlign: TextAlign.center))),
+                  Expanded(flex: 1, child: columnWithPadding(child: Text(qtyFormatter.format(item.quantity), style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.end))),
+                  Expanded(flex: 3, child: columnWithPadding(child: Text(item.name, style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.start))),
+                  Expanded(flex: 2, child: columnWithPadding(child: Text(amountFormatter.format(item.total), style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.center))),
                   Expanded(flex: 2, child: actionWidget),
                 ],
               ),
