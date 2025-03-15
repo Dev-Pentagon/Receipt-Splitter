94852f20a73520d5958ea67efcb1eb2f6c5b91f8
Wunna Kyaw <wunnakyaw.developer@gmail.com>
2025-03-15 11:29:21 +0630

    add table_widget.dart

---
+++ receipt_splitter/lib/module/receipt_detail/common/table_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:receipt_splitter/config/app_config.dart';
+import 'package:receipt_splitter/model/menu_item.dart';
+
+import '../../../constants/strings.dart';
+
+class TableWidget extends StatelessWidget {
+  final List<MenuItem> items;
+  final String actionName;
+  final Widget actionWidget;
+  const TableWidget({super.key, required this.items, required this.actionName, required this.actionWidget});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Header Row
+        Row(
+          children: [
+            Expanded(flex: 1, child: columnWithPadding(child: Text(QTY, style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.end), vertical: 0),),
+            Expanded(flex: 3, child: columnWithPadding(child: Text(NAME, style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.start), vertical: 0)),
+            Expanded(flex: 2, child: columnWithPadding(child: Text(TOTAL, style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.center), vertical: 0)),
+            Expanded(flex: 2, child: columnWithPadding(child: Text(actionName, style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.center), vertical: 0)),
+          ],
+        ),
+        const Divider(),
+        // Data Rows
+        ...items.map(
+          (item) => Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(flex: 1, child: columnWithPadding(child: Text(qtyFormatter.format(item.quantity), style: Theme.of(context).textTheme.labelMedium, textAlign: TextAlign.end))),
+                  Expanded(flex: 3, child: columnWithPadding(child: Text(item.name, style: Theme.of(context).textTheme.labelMedium, textAlign: TextAlign.start))),
+                  Expanded(flex: 2, child: columnWithPadding(child: Text(amountFormatter.format(item.total), style: Theme.of(context).textTheme.labelMedium, textAlign: TextAlign.center))),
+                  Expanded(flex: 2, child: actionWidget),
+                ],
+              ),
+              const Divider(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget columnWithPadding({required Widget child, double vertical = 20.0, double horizontal = 8.0}) {
+    return Padding(padding: EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical), child: child);
+  }
+}
